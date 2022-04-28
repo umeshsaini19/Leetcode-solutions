78f4d998e8cf3d4e81102383a6510ad3849c00be
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-04-28 11:25:04 +0530

    Time: 11 ms (27.74%), Space: 15.6 MB (5.69%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -35,31 +35,55 @@ public:
 //             v.push_back(c);
 
 //         return v;
-     map<int,map<int,multiset<int>>> m;
-    void solve(TreeNode* root,int col,int row)
-    {
-        if(!root)return;
-        m[col][row].insert(root->val);
-        solve(root->left,col-1,row+1);
-        solve(root->right,col+1,row+1);
+    
+    
+    //second  
+//      map<int,map<int,multiset<int>>> m;
+//     void solve(TreeNode* root,int col,int row)
+//     {
+//         if(!root)return;
+//         m[col][row].insert(root->val);
+//         solve(root->left,col-1,row+1);
+//         solve(root->right,col+1,row+1);
+//     }
+// public:
+//     vector<vector<int>> verticalTraversal(TreeNode* root) {
+//         vector<vector<int>> v;
+//         solve(root,0,0);
+//         for(auto m1:m)
+//         {
+//             v.push_back(vector<int>());
+//             for(auto m2:m1.second)
+//             {
+//                 for(auto m3:m2.second)
+//                 {
+//                     v.back().push_back(m3);
+//                 }
+//             }
+//         }
+//         return v;    
+        
+//     }
+     map<int, map<int, multiset<int>>>nodes;
+    void dfs(TreeNode* root, int x , int y){
+        if(root==NULL) return;
+        nodes[x][y].insert(root->val);
+        dfs(root->left, x-1, y+1);
+        dfs(root->right, x+1, y+1);
     }
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>> v;
-        solve(root,0,0);
-        for(auto m1:m)
-        {
-            v.push_back(vector<int>());
-            for(auto m2:m1.second)
-            {
-                for(auto m3:m2.second)
-                {
-                    v.back().push_back(m3);
-                }
-            }
+        dfs(root, 0, 0);
+        vector<vector<int>>ans;
+        for(auto p1:nodes){
+            vector<int>temp;
+            for(auto p2:p1.second)
+                temp.insert(temp.end(), p2.second.begin(), p2.second.end());//temp.end la mtlb last me lgayiye value p2 vali shuru se end tak 
+         
+         
+            ans.push_back(temp);
         }
-        return v;    
-        
+        return ans;
     }
     
     
