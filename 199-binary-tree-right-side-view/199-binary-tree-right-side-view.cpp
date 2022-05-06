7f7ade2290484e3c236355ffac8a38c663b3b083
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-06 17:17:39 +0530

    Time: 3 ms (79.78%), Space: 12 MB (69.48%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -14,7 +14,8 @@ public:
     void solve(TreeNode*root,int level,vector<int>&res){
         if(root==NULL) return ;
         
-        if(level==res.size()) res.push_back(root->val);
+        if(level==res.size()) res.push_back(root->val);   //left ke liye kam ni krega bcz vector ka size pehle hi 3 ho chuka he 
+        //so if condtition work ni kregi
         solve(root->right,level+1,res);
         solve(root->left,level+1,res);
     }
