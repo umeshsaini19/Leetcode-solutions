a8229138554baa2f8f35422f35ee078c147eaf87
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-08 10:01:42 +0530

    Time: 57 ms (36.61%), Space: 18.9 MB (85.42%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-   vector<vector<int>> ans;
-        if(intervals.size()==0)return ans;
-    sort(intervals.begin(),intervals.end());
-        ans.push_back(intervals[0]);
+   vector<vector<int>>ans;
+        if(intervals.size()==0) return ans;
+        sort(intervals.begin(),intervals.end());
         int j=0;
-        for(int i=1;i<intervals.size();i++)
-        {
-            if(ans[j][1]>=intervals[i][0])
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(ans[j][1]>=intervals[i][0]){
                 ans[j][1]=max(ans[j][1],intervals[i][1]);
-            else
-            {
+            }
+            else{
                 j++;
                 ans.push_back(intervals[i]);
             }
         }
         return ans;
+        
+    //       vector<vector<int>> ans;
+    //     if(intervals.size()==0)return ans;
+    // sort(intervals.begin(),intervals.end());
+    //     ans.push_back(intervals[0]);
+    //     int j=0;
+    //     for(int i=1;i<intervals.size();i++)
+    //     {
+    //         if(ans[j][1]>=intervals[i][0])
+    //             ans[j][1]=max(ans[j][1],intervals[i][1]);
+    //         else
+    //         {
+    //             j++;
+    //             ans.push_back(intervals[i]);
+    //         }
+    //     }
+    //     return ans;
     }
 };
