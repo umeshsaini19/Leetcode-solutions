428ca34c7ad3dc66c6e6261bc0608b92b929f4f4
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-09 13:51:25 +0530

    Time: 20 ms (78.59%), Space: 19.5 MB (59.98%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-         int n = nums.size();
-		//ek tarah se jais evoting krenge agr khudko vote milra toh count+1 agr dusre ko milra toh count-1;
-        //and agr count 0 hogya mtlb brabr he abhi toh jispe last khtm huya voh winner lgra
-       int hero=nums[0];
-        int power=1;
-        for(int i=1;i<n;i++){
-            if(nums[i]==hero)
-                power++;
-            else 
-                power=power-1;
-            
-            if(power==0){
-                hero=nums[i];
-                power=1;
+         int n = nums.size(); // extracting the size of the array
+        
+        int hero = nums[0]; // Intially assume our majority element of index zero 
+        int power = 1; // count of majority element
+        
+        for(int i = 1; i < n; i++) // start traversing from the array
+        {
+            if(nums[i] == hero) // if same element encounters
+            {
+                power++; // increase the power of element
+            }
+            else // if not same element
+            {
+                // decrease power 1, as it used to kill the villian
+                power = power - 1; 
+                
+                // if power of our hero becomes zero at any point,
+                // we say may be your are not our hero, so assign new hero
+                if(power == 0)
+                {
+                    hero = nums[i]; // assigning hero
+                    power = 1; // make it's power to 1
+                }
             }
         }
-        return hero;
+            return hero;
     }
 };
