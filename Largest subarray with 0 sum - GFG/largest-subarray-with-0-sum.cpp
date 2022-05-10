294b11e2d3857e56ba2a8fc305d171a91bc37a81
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-10 21:57:16 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
@@ -12,25 +12,41 @@ class Solution{
     public:
     int maxLen(vector<int>&A, int n)
     {   
-        unordered_map<int,int>mpp;
-       int maxi = 0;
-       int sum = 0;
-       for(int i = 0;i<n;i++){
-           sum += A[i];
-           if(sum == 0){
-               maxi = i+1; //storing longestSubarray
-           }
-           else{
-               if(mpp.find(sum) != mpp.end()){ //if there is prefix of element in hashap
-                   maxi = max(maxi, i-mpp[sum]);   //get that index and get the length
-               }
-               else{
-                   mpp[sum] = i;
-               }
-           }
+        unordered_map<int,int>mp;
+        int maxi=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+A[i];
+            if(sum==0)
+            maxi=i+1;                 
+            else{
+                
+             if(mp.find(sum)!=mp.end()){
+                 maxi=max(maxi,i-mp[sum]);
+             }
+             else mp[sum]=i;
+             
+            }  
+        }return maxi;
+    //     unordered_map<int,int>mpp;
+    //   int maxi = 0;
+    //   int sum = 0;
+    //   for(int i = 0;i<n;i++){
+    //       sum += A[i];
+    //       if(sum == 0){
+    //           maxi = i+1; //storing longestSubarray
+    //       }
+    //       else{
+    //           if(mpp.find(sum) != mpp.end()){ //if there is prefix of element in hashap
+    //               maxi = max(maxi, i-mpp[sum]);   //get that index and get the length
+    //           }
+    //           else{
+    //               mpp[sum] = i;
+    //           }
+    //       }
            
-       }
-       return maxi;
+    //   }
+    //   return maxi;
     }
 };
 
