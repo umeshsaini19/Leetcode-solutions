16deacb27ae2fdc64bd379a88435a084d2f229c2
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-19 19:33:40 +0530

    Time: 3 ms (99.67%), Space: 18.4 MB (36.95%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -3,18 +3,11 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int count=0;
         for(int i=1;i<nums.size();i++){
-            if(nums[count]!=nums[i])
+            if(nums[count]!=nums[i])  //agr dono unequal he toh same value same pe chd rhi he and agr dono equal he toh agli value pechle pe chd rhi he 
                 count++;
+            
             nums[count]=nums[i];
          
         }return count+1;
     }
 };
-
-// int left = 0;
-//         for(int right =1; right< nums.size(); right++){
-//            if(nums[left] != nums[right])
-//                left++;
-//                nums[left] = nums[right];
-//            }
-//     return left+1;
