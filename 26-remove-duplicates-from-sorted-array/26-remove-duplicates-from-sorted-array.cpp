4131a1e3599fdbb07cd3ddb7072786d4cb3d24ca
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-20 19:26:49 +0530

    Time: 33 ms (11.95%), Space: 18.4 MB (36.87%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -3,11 +3,18 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int count=0;
         for(int i=1;i<nums.size();i++){
-            if(nums[count]!=nums[i])  //agr dono unequal he toh same value same pe chd rhi he and agr dono equal he toh agli value pechle pe chd rhi he 
+            if(nums[count]!=nums[i]){  //agr dono unequal he toh same value same pe chd rhi he and agr dono equal he toh agli value pechle pe chd rhi he 
                 count++;
             
-            nums[count]=nums[i];
+            nums[count]=nums[i]; }
          
         }return count+1;
     }
 };
+  //agr hum dono same he toh count ni bdega ek ke upr aek a jayega and agr hum dono same ni he toh count bdaodo and same ke same same a jayega 
+
+// 1 1 2 2 3 3 3   j++ =0
+//                 1 2 2 3 3 
+//     1 1 2   =0;
+// nums[0]=nums[1];
+    
