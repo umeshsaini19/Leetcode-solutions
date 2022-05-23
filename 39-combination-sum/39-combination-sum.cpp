269e2df4e9fe6d41e3c435de40a1657766ef9e1d
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-23 10:10:47 +0530

    Time: 15 ms (48.52%), Space: 10.8 MB (72.55%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     
     void solve( vector<vector<int>>&ans, vector<int>&v,int i,int target,vector<int>&arr){
-        if(i==arr.size()){
+        if(i==arr.size()){            //agr index last pe punch gya he toh 2 possibility he ya toh target hi 0 hogya he mtlb hum sahi he and me push krdo else vapis jao hum galt agye return lrdp
             if(target==0){
                 ans.push_back(v);
             }return ;
         }
-        if(arr[i]<=target){
-            v.push_back(arr[i]);
-            solve(ans,v,i,target-arr[i],arr);
-            v.pop_back();
+        if(arr[i]<=target){    //agr value target se choti he tbhi push kro na ni toh kya lroge push krke
+            v.push_back(arr[i]);    
+            solve(ans,v,i,target-arr[i],arr); //pick element of same index vala case
+            v.pop_back();           //jab return krenge toh jo eleemtn pick kiya he use remove v krna pdega next cases ke liye
         }
-        solve(ans,v,i+1,target,arr);
+        solve(ans,v,i+1,target,arr);         //yeh index bddha rhe he pr element pick ni kr rhe hum log
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
