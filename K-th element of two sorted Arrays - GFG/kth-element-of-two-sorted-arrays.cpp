70bf7245c1b4810d2bf7682cf4f0c4c5bc88cef7
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-28 20:51:13 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -6,33 +6,39 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    int kthElement(int arr1[], int arr2[], int m, int n, int k){
-    
-    // if(n>m){
-    // return kthElement( arr2,  arr1,  m,  n,  k);
+    int kthElement(int arr1[], int arr2[], int m, int n, int k) {
+        //dont know why yeh neche vala solution yahan kam kyu ni kr rha he 
+        
+    // if(m > n) {
+    //     return kthElement(arr2, arr1, n, m, k); 
     // }
-    // int low=max(0,k-n);
-    // int high=min(m,k);
-    // while(low<=high){
-    //     int cut1=low+(high-low)/2;
-    //     int cut2=k-cut1;
-    //     int l1,l2=INT_MIN;
-    //     int r1,r2=INT_MAX;
-    //      l1=arr1[cut1-1];
-    //      l2=arr2[cut2-1];
-    //      r1=arr1[cut1];
-    //      r2=arr2[cut2];
-    //      if(l1<=r2 && l2<=r1){
-    //          return max(l1,l2);
-    //      }
-    //      else if(l1>r2){
-    //          high=cut1-1;
-    //      }
-    //      else low=cut1+1;
+        
+    // int low = max(0,k-m), high = min(k,n);
+        
+    // while(low <= high) {
+    //     int cut1 = (low + high) >> 1; 
+    //     int cut2 = k - cut1; 
+    //     int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1]; 
+    //     int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
+    //     int r1 = cut1 == n ? INT_MAX : arr1[cut1]; 
+    //     int r2 = cut2 == m ? INT_MAX : arr2[cut2]; 
+            
+    //     if(l1 <= r2 && l2 <= r1) {
+    //         return max(l1, l2);
+    //     }
+    //     else if (l1 > r2) {
+    //         high = cut1 - 1;
+    //     }
+    //     else {
+    //         low = cut1 + 1; 
+    //     }
     // }
-    // // return 1;
+    // return 1; 
     
-   int sorted1[m + n];
+ //   yahan se dusra solution start hota he 
+    
+    
+  int sorted1[m + n];
 	int i = 0, j = 0, d = 0;
 	while (i < m && j < n)
 	{
