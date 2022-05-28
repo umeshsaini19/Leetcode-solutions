f1697a275a6aae9773bad102266daffc6a7df956
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-28 13:27:29 +0530

    Time: 3 ms (85.46%), Space: 11.1 MB (30.09%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    //iske peche intution yehi he ki hme pta he bnary search sorted pe hi lgta he so hmne pehle mid find krlia fir uske badh pehle dekha sorted konca he jo vala part sorted he uski trf hoge hum log and check krli aagr usme ni he toh unsorted me jake firse sorted hi dunda hmne and usme check kiya agr usme he toh thik ni he toh unsorted me jake firse ssorted dundo
     int search(vector<int>& nums, int target) {
          int low =0 , high = nums.size()-1;
         int mid;
