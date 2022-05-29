4513bfa37183b94fc7483b4f986626e1c1a2565c
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-29 16:25:47 +0530

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
@@ -14,41 +14,73 @@ class Solution
     //Function to find minimum number of pages.
     bool isPossible(int barrier, int A[], int N, int M)
     {
-        int pages=0, students=1 ;
-        for(int i=0 ; i<N ; i++)
-        {
-            if(barrier<A[i]) return false ;
-            if(pages+A[i]>barrier)
-            {
-                students++ ;
-                pages=0 ;
-                pages+=A[i] ;
-            }
-            else
-                pages+=A[i] ;
-        }
+        int pages=0,student=1;
         
-        return students>M ? false : true ;
-    }
+        for(int i=0;i<N;i++){
+            if(A[i]>barrier) return false;
+            if(pages+A[i]>barrier){
+                student+=1;
+                pages=0;
+                pages+=A[i];
+            }
+            else{
+                pages+=A[i];
+            }
+        }
+        if(student>M) return false;
+        else return true;}
+    //     int pages=0, students=1 ;
+    //     for(int i=0 ; i<N ; i++)
+    //     {
+    //         if(barrier<A[i]) return false ;
+    //         if(pages+A[i]>barrier)
+    //         {
+    //             students++ ;
+    //             pages=0 ;
+    //             pages+=A[i] ;
+    //         }
+    //         else
+    //             pages+=A[i] ;
+    //     }
+        
+    //     return students>M ? false : true ;
+    // }
     //Function to find minimum number of pages.
     int findPages(int A[], int N, int M) 
     {
-        int ans=-1, low=INT_MAX, high=0 ;
-        for(int i=0 ; i<N ; i++)
-            low=min(low,A[i]), high+=A[i] ;
-            
-        while(low<=high)
-        {
-            int mid = (low+high)/2 ;
-            if(isPossible(mid,A,N,M))
-            {
-                ans=mid ;
-                high=mid-1 ;
-            }
-            else low=mid+1 ;
+        int ans=-1,low=INT_MAX;
+        int high=0;
+        for(int i=0;i<N;i++){
+            low=min(low,A[i]),high+=A[i];
         }
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isPossible(mid,A,N,M)){
+                ans=mid;
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+        }return ans;
         
-        return ans ;
+        
+        // int ans=-1, low=INT_MAX, high=0 ;
+        // for(int i=0 ; i<N ; i++)
+        //     low=min(low,A[i]), high+=A[i] ;
+            
+        // while(low<=high)
+        // {
+        //     int mid = (low+high)/2 ;
+        //     if(isPossible(mid,A,N,M))
+        //     {
+        //         ans=mid ;
+        //         high=mid-1 ;
+        //     }
+        //     else low=mid+1 ;
+        // }
+        
+        // return ans ;
     }
 };
 
