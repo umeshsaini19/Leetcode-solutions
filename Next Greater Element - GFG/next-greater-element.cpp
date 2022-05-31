79d67c6b1acfdbe517d68706e02993f6fa9ab8fe
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-05-31 10:49:43 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -9,6 +9,8 @@ class Solution
     public:
     //Function to find the next greater element for each element of the array.
     vector<long long> nextLargerElement(vector<long long> arr, int n){
+        
+        //iske ilava circular array vala bhi he voh bhi dekhlena 
         vector<long long>v;
         stack<long long>s;
         for(int i=n-1;i>=0;i--){
@@ -34,37 +36,31 @@ class Solution
         
         
         
+        //yeh circular vala he striver bhaiya se sikha he
+//          stack<int> s;
+//     vector<int> ans(n);
+//     for (int i = 2 * n - 1; i >= 0; i--) {
+//         while (!s.empty() && a[i % n] >= s.top())
+//             s.pop();
+//         if (i < n) {
+//             if (!s.empty())
+//                 ans[i] = s.top();
+//             else
+//                 ans[i] = -1;
+//         }
+//         s.push(a[i % n]);
+//     }
+//     for (int i = 0; i < n; i++)
+//         cout << ans[i] << " ";
+// }
+ 
         
         
         
         
     
         
-    //   vector<long long>v;
-    //   stack<long long>s;
-    //   for(int i=n-1;i>=0;i--){
-    //       if(s.size()==0)
-    //       v.push_back(-1);
-           
-    //       else if(s.size()>0 && s.top()>arr[i]){
-    //           v.push_back(s.top());
-               
-    //       }
-    //       else if(s.size()>0&& s.top()<=arr[i]){
-    //           while(s.size()>0 && s.top()<=arr[i]){
-    //               s.pop();
-    //           }
-    //           if(s.size()==0){
-    //               v.push_back(-1);
-    //           }
-    //           else {
-    //               v.push_back(s.top());
-    //           }
-    //       }
-    //       s.push(arr[i]);
-    //   }
-    //     reverse(v.begin(),v.end());
-    //     return v;
+   
     }
 };
 
