174a8fc76194e5770a85a17f56efed5b21d9ec64
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-02 12:47:27 +0530

    Time: 19 ms (20.33%), Space: 7.6 MB (53.87%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,6 +1,50 @@
 class Solution {
 public:
+    // string reverseWords(string s) {
+        
+        //yeh neche vala code lgbhag har ek test code ke liye cjhal kayega ek adhe ko chod  kr yeh sbse optimal he pr sbse last vala sbse oehla dekhn ahe agr smjna he toh
+//    int left = 0;
+//     int right = s.length()-1;
     
+//     string temp="";
+//     string ans="";
+    
+//     //Iterate the string and keep on adding to form a word
+//     //If empty space is encountered then add the current word to the result
+//     while (left <= right) {
+//         char ch= s[left];
+//         if (ch != ' ') {
+//             temp += ch;
+//         } else if (ch == ' ') {
+//             if (ans!="") ans = temp + " " + ans;
+//             else ans = temp;
+//             temp = "";
+//         }
+//         left++;
+//     }
+    
+//     //If not empty string then add to the result(Last word is added)
+//     if (temp!="") {
+//         if (ans!="") ans = temp + " " + ans;
+//         else ans = temp;
+//     }
+    
+//     return ans; 
+// }
+// };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //yeh stack vali approach eeh
+    
+     
     // yeh logic he jaise word he =>  my name  toh jaise m aya hmne khali string me concetenate krdia fir y aa m ke sath concetenate krdia jaise hi space ai hmne skip krdi avoh part and my ko stack me daldia then again same name ke sath hi fir jab vapis nikalna he ek iteration ki stack ki and check kiya stack empty he agr ni he mtlb space ke badh top pe dusra element he ek element nikalo space deke dusra nikal do and asatha me pop krte rho
     string reverseWords(string s) {
         if(s.size()==0) return s;
@@ -20,30 +64,13 @@ public:
             if(!st.empty()) result+=" ";
         }
         return result;
-        
-        
-        
-        
-        
-        
-        
-        
-        
- // if(s.size() == 0) return s;
- //        stack<string> stack;
- //        string result;
- //        for(int i=0; i<s.size(); i++) {
- //            string word;
- //            if(s[i]==' ') continue; //skip spaces
- //            while(i<s.size() && s[i]!=' ' ) { //store continuous letters into word
- //                word += s[i]; i++;
- //            }
- //            stack.push(word); //push word to the stack
- //        }
- //        while(!stack.empty()) {
- //            result += stack.top(); stack.pop();
- //            if(!stack.empty()) result += " ";
- //        }
- //        return result;
     }
-}; 
+};
+        
+        
+        
+        
+        
+        
+        
+        
