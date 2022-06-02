a0690756723055a7d68b6c8bc6997740349f4d4b
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-02 17:12:15 +0530

    Time: 601 ms (20.25%), Space: 11.5 MB (54.81%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+      int n = s.size();
+        int dp[n][n];
+        memset(dp, 0, sizeof dp);
+        
+        int len = 0, start = 0;
+        for (int g = 0; g < n; ++g) {
+            for (int i = 0, j = g; j < n; ++i, ++j) {
+                if (g == 0)
+                    dp[i][j] = true;
+                else if (g == 1){ if(s[i] == s[j]){
+                    dp[i][j]=true;}
+                else {
+                    dp[i][j]=false;
+                }
+            }
+                else
+                {if(s[i] == s[j] && dp[i + 1][j - 1]==true){
+                    dp[i][j] = true;
+                }else{
+                    dp[i][j]=false;
+                }
+                }
+                if (dp[i][j]) {
+                    len = g + 1;
+                    start = i;
+                }
+            }
+        }
+            
+        return s.substr(start, len);
+    }
+};
