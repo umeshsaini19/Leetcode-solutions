0d6674a447613f9f2f2b6c90d01e97168415418f
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-02 12:12:26 +0530

    Time: 11 ms (38.90%), Space: 7.4 MB (63.78%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     string reverseWords(string s) {
- if(s.size() == 0) return s;
-        stack<string> stack;
+        if(s.size()==0) return s;
+        stack<string>st;
         string result;
-        for(int i=0; i<s.size(); i++) {
+        for(int i=0;i<s.size();i++){
             string word;
-            if(s[i]==' ') continue; //skip spaces
-            while(i<s.size() && s[i]!=' ' ) { //store continuous letters into word
-                word += s[i]; i++;
-            }
-            stack.push(word); //push word to the stack
+            if(s[i]==' ') continue;
+            
+            while(i<s.size() && s[i]!=' ' ){
+                word+=s[i];i++;}
+             st.push(word);
         }
-        while(!stack.empty()) {
-            result += stack.top(); stack.pop();
-            if(!stack.empty()) result += " ";
+        while(!st.empty()){
+            result+=st.top();
+            st.pop();
+            if(!st.empty()) result+=" ";
         }
         return result;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+ // if(s.size() == 0) return s;
+ //        stack<string> stack;
+ //        string result;
+ //        for(int i=0; i<s.size(); i++) {
+ //            string word;
+ //            if(s[i]==' ') continue; //skip spaces
+ //            while(i<s.size() && s[i]!=' ' ) { //store continuous letters into word
+ //                word += s[i]; i++;
+ //            }
+ //            stack.push(word); //push word to the stack
+ //        }
+ //        while(!stack.empty()) {
+ //            result += stack.top(); stack.pop();
+ //            if(!stack.empty()) result += " ";
+ //        }
+ //        return result;
     }
 }; 
