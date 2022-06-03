f57a5ccf4783c6c51e784fe000b0ed2869d340b9
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-03 09:34:43 +0530

    Time: 23 ms (15.82%), Space: 7.3 MB (46.68%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,9 +3,28 @@ public:
     bool isAnagram(string s, string t) {
      if(s.size()!=t.size()) return false;
         
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        unordered_map<char,int>mp;
+        for( auto c:s)
+            mp[c]++;
+        for(auto c:t){
+            mp[c]--;
+            if(mp[c]<0) return false;
+        }
+        return true;
+      
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
         
-        return s==t;
+//         return s==t;
     }
 };
+//  if (s.size()!=t.size()) return false;
+        
+// 		unordered_map <char, int> umap;
+//         for (char c:s) umap[c]++;
+//         for(char c:t) {
+//             umap[c]--;
+//             if (umap[c]<0) return false;
+//         }
+        
+//         return true;
