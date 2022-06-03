eda035721c080d214962bb094524aae6be502b5e
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-03 11:09:56 +0530

    Time: 4 ms (27.11%), Space: 6.3 MB (32.29%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -2,60 +2,56 @@ class Solution {
 public:
     int compareVersion(string version1, string version2) {
         
-        int i=0,j=0,n1=version1.size(),n2=version2.size(),num1=0,num2=0;
-        while(i<n1 || j<n2){
-            while(i<n1 && version1[i]!='.'){
-                num1=num1*10+(version1[i]-48);
+  //      https://leetcode.com/problems/compare-version-numbers/discuss/1797603/C%2B%2Bor-Detailed-Explanation-w-DRY-RUN-or-Faster-than-100.00-orCommented-code
+        string s1=version1;
+        string s2=version2;
+      int n1 = s1.length(); // extracting length of string s1
+        int n2 = s2.length(); //extracting length of string s2
+        
+        int i = 0, j = 0; // variables(pointers) used for moving
+        
+        int number1 = 0, number2 = 0;  // numbers that generated between any two dots of the strings
+        
+        // start traversing from string 1 and string 2
+        while(i < n1 || j < n2)
+        {
+            // generating number between dots for string s1
+            while(i < n1 && s1[i] != '.')
+            {
+                number1 = number1 * 10 + (s1[i] - '0');
                 i++;
-                
-            }
-            while(j<n2 && version2[j]!='.'){
-                num2=num2*10+(version2[j]-48);
-                j++;
-                
             }
             
-            if(num1>num2) return 1;
-            if(num2>num1) return -1;
-            num1=0,num2=0;
+            // generating number between dots for string s2
+            while(j < n2 && s2[j] != '.')
+            {
+                number2 = number2 * 10 + (s2[j] - '0');
+                j++;
+            }
+            
+            // if number1 is greater than number2, from here return 1
+            if(number1 > number2)
+            {
+                return 1;
+            }
+            
+             // if number1 is less than number2, from here return -1
+            if(number1 < number2)
+            {
+                return -1;
+            }
+            
+            // if equal then we have to proceed further
+            // again give numbers to zero, as we again generate numbers b/w dots 
+            number1 = 0, number2 = 0;
+            
+            // move both pointers
             i++;
             j++;
-        }return 0;
+        }
         
-        
-        
-        
-        
-        
-        
-        
-        
-//          int i = 0, j = 0, n1 = version1.size(), n2 = version2.size(), num1 = 0, num2 = 0; 
-//          while(i<n1 || j<n2)
-//         {
-//             while(i<n1 && version1[i]!='.')
-//             {
-//                 num1 = num1*10+(version1[i]-48);
-//                 i++;
-//             }
-//             while(j<n2 && version2[j]!='.')
-//             {
-//                 num2 = num2*10+(version2[j]-48);
-//                 j++;
-//             }
-//             if(num1>num2)
-//             {
-//                 return 1;
-//             }
-//             if(num1 < num2) 
-//             {
-//                 return -1;
-//             }
-//             i++;
-//             j++;
-//             num1 = 0;
-//             num2 = 0;
-//         }
-//         return 0; 
+        // after traversing whole string, if all the versions are equal, that means
+        // strings are equal, so return 0
+        return 0;
     }
 };
