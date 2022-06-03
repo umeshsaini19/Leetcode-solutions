e49ceea18f96ef859db7260427a34f60d06336fd
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-03 09:38:20 +0530

    Time: 8 ms (79.83%), Space: 7.2 MB (78.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,23 +3,24 @@ public:
     
     //cmnt vala part hta do tn second method he 
     bool isAnagram(string s, string t) {
-     if(s.size()!=t.size()) return false;
+//      if(s.size()!=t.size()) return false;
         
-        unordered_map<char,int>mp;
-        for( auto c:s)
-            mp[c]++;
-        for(auto c:t){
-            mp[c]--;
-            if(mp[c]<0) return false;
-        }
-        return true;
+//         unordered_map<char,int>mp;
+//         for( auto c:s)
+//             mp[c]++;
+//         for(auto c:t){
+//             mp[c]--;
+//             if(mp[c]<0) return false;
+//         }
+//         return true;
+        
+        
       
 //         sort(s.begin(),s.end());
 //         sort(t.begin(),t.end());
         
 //         return s==t;
-    }
-};
+
 //  if (s.size()!=t.size()) return false;
         
 // 		unordered_map <char, int> umap;
@@ -30,3 +31,22 @@ public:
 //         }
         
 //         return true;
+        
+        //3rd soluion
+
+ if(s.size() != t.size()) return false;
+        
+        unordered_map<char, int> freq;
+        
+        for(int i = 0; i < s.size(); i++){
+            freq[s[i]]++;
+            freq[t[i]]--;
+        }
+        
+        for(auto f : freq){
+            if(f.second != 0) return false;
+        }
+        
+        return true;
+            }
+};
