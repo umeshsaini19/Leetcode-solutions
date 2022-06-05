2a566d49ef25574cd8ffe893d0f3aad7f65ab8c3
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-05 11:24:12 +0530

    Time: 12 ms (33.33%), Space: 9.8 MB (91.67%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.cpp
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minMaxGame(vector<int>& nums) {
-        int N = nums.size();
-         while(N!=1){
-             N=N/2;
-             for(int i=0;i<N;i++){
-                 nums[i]=(i%2==0)?min(nums[2*i],nums[2*i+1]):max(nums[2*i],nums[2*i+1]);
-             }
-         }        
-        return nums[0];
+        // int N = nums.size();
+        //  while(N!=1){
+        //      N=N/2;
+        //      for(int i=0;i<N;i++){
+        //          nums[i]=(i%2==0)?min(nums[2*i],nums[2*i+1]):max(nums[2*i],nums[2*i+1]);
+        //      }
+        //  }        
+        // return nums[0];
         
 
 //        while (N!=1) {
@@ -18,6 +18,22 @@ public:
 //                 nums[i] = (i%2 == 0) ? min(nums[2*i], nums[2*i+1]) : max(nums[2*i], nums[2*i+1]);
         
 //         return nums[0];
+     int n=nums.size(); 
+        if(n==1){ 
+            return nums[0]; 
+        } 
+        vector<int> newNUM(n/2,0);
+        for(int i=0;i<n/2;i++){ 
+            if(i%2==0){ 
+                newNUM[i]=min(nums[2 * i], nums[2 * i + 1]); 
+            } 
+            else{ 
+              newNUM[i]=max(nums[2 * i], nums[2 * i + 1]); 
+            } 
+        } 
+         
+        int ans=minMaxGame(newNUM); 
+        return ans; 
     }
     
 };
