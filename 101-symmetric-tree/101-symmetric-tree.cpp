43fdaafd3c064e403e6d93fae74bad4248628522
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-08 18:43:19 +0530

    Time: 13 ms (17.23%), Space: 16.4 MB (49.46%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,15 +11,28 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode*p,TreeNode*q){
-        if(p==NULL and q==NULL) return true;
-        if(p==NULL|| q==NULL) return false;
-        if(p->val!=q->val) return false;
-        return solve(p->left,q->right ) and solve(p->right,q->left);
+//     bool solve(TreeNode*p,TreeNode*q){
+//         if(p==NULL and q==NULL) return true;
+//         if(p==NULL|| q==NULL) return false;
+//        // if(p->val!=q->val) return false;
+//         else if return solve(p->left,q->right ) and solve(p->right,q->left);
+//         else return false;
+        
+//     }
+//     bool isSymmetric(TreeNode* root) {
+//         if(root==NULL) return true;
+//         return solve(root->left,root->right);
+        
+    bool isMirror(TreeNode* root1,TreeNode *root2){
+        if(root1==NULL && root2==NULL)
+            return true;
+        if(root1 && root2 && root1->val == root2->val)
+            return isMirror(root1->left,root2->right) && isMirror(root1->right,root2->left);
+        
+        return false;
     }
     bool isSymmetric(TreeNode* root) {
-        if(root==NULL) return true;
-        return solve(root->left,root->right);
-        
+        return isMirror(root,root);
+    
     }
 };
