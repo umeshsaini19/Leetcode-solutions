126358a37f606d1a20a52aa4837d41c182a645e4
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-11 21:24:24 +0530

    Time: 123 ms (5.24%), Space: 13.7 MB (72.45%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -6,9 +6,13 @@ public:
 	    long long end=1;
 	    long long ans=INT_MIN;
 	   for(int i=0;i<n;i++){
+           //age and peche dono side se traverse kr rhe he 
 	       start=start*nums[i];
+           cout<<start<<" ";
 	       end=end*nums[n-i-1];
+           cout<<end<<" ";
 	       ans=max(ans,max(start,end));
+           cout<<ans<<" ";
 	       
 	       if(start==0)
 	       start=1;
