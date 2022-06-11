7e02f1fe895fbb9f6c188e6f742d1fb6ce1e9e84
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-11 21:25:10 +0530

    Time: 167 ms (5.24%), Space: 13.8 MB (26.90%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        //intuition of this code amazing one
+  //      https://leetcode.com/problems/maximum-product-subarray/discuss/1608800/C%2B%2B-or-Discussion-in-detail-or-Easy-to-understand
         int n=nums.size();
        long long start=1;
 	    long long end=1;
@@ -22,3 +24,5 @@ public:
   return ans;
     }
 };
+
+// If the count is odd, then we would want to exclude one -ve number from our product, so that the product gets maximised. So, now the question is, which -ve number to exclude? Example ---> arr={-2,-3,-1,-4,-5} which number should be excluded ? On observing it , we should get one fact clear, that the number which is going to get ignored is either going to be the first one or the last one.
