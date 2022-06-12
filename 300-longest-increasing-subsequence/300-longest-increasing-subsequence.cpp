10f99ab6362e0cb2df7cdac47d635f73dd7aeb6e
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-12 12:18:24 +0530

    Time: 9 ms (90.85%), Space: 10.4 MB (65.62%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,1);
-        int ans=1;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[i],1+dp[j]);         //yeh max esleye manlo 5,4,11,16 he and 5 ,4,11,16 sabki original dp abhi 1 hi he jaise manlo 16 ki bari ai toh 11 usse pehle process ho chuka he and 11 ko 2 mila huya he and jab 16 ki bari ai toh 16 ki dp 5,16 se 2 hogi and same 4,16 se 2 and jab 11 ki bari ai 16 ki toh 4.11.16 aise consider huya and 11 ki vajah se 16 ko 3 milega esleye har bari previous me bhi 1 add krke age bdhna pdega tbhi pta lgega
-                    ans=max(ans,dp[i]);
-                }
+        vector<int>v;
+        v.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>v.back()){
+                v.push_back(nums[i]);
+            }
+            else {
+                int ind=lower_bound(v.begin(),v.end(),nums[i])-v.begin();//yeh return krega ki jo element he uske just next bdi vali value like 5,4 toh jaise hi lower bound me 4 gya toh isne 5 ka index yani 0 retun krdena toh value usme a jayegi
+       v[ind]=nums[i];   
             }
         }
-        return ans;
+        return v.size();
     }
 };
-
-// int ans = 1, n = size(nums);
-//         vector<int> dp(n, 1);
-//         for(int i = 0; i < n; i++) 
-//             for(int j = 0; j < i; j++) 
-//                 if(nums[i] > nums[j]) 
-// 				    dp[i] = max(dp[i], dp[j] + 1), ans = max(ans, dp[i]);
-//         return ans;
