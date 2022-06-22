ed0c359b56060c7b9e5616461d22ee2a08cacd90
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-22 13:36:20 +0530

    Time: 20 ms (47.25%), Space: 17.4 MB (84.72%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -19,49 +19,64 @@ class Solution {
 public:
     //function returning node and take input as node, 
     //finds the next node for each node.
-    Node* findNext(Node* root)
+    Node *findNext(Node *root)
     {
-        //simple if root is null, next node will also be null.
-        if(!root) return nullptr;
-        //if node's left exist, then we will return node's left.
-        if(root->left) return root->left;
-        //if this if condition fails to execute, this means, there is no left
-        //so we will simply return node's right if it exist.
-        if(root->right) return root->right;
+        if(!root) return NULL;
         
-        //suppose neither node left exist nor right,
-        //then we will findnext on root's next.
-        //recusion fairy.
+        if(root -> left) return root -> left;
+        if(root -> right) return root -> right;
+        
         return findNext(root->next);
     }
-    //dfs function 
-    void dfs(Node* root)
-    {
-        if(!root) return ; //return if root is null
-        if(root->left) //if root's left exist
-            //assign it's next based on weather root's right exist or not.
-            root->left->next = root->right ? root->right : findNext(root->next); 
-        //if root's right exist, no issue, make it the next of root's left
-        //else whatever findnext(root->next) return, make that the next of 
-        //root's left.
-        //and if root's right exists
-        if(root->right)
-        //root's right next eill simply be whatever findnext returns on root->next
-            root->right->next = findNext(root->next);
-        
-        //now recursivly call dfs on root's right as well as root's left.
-        // calling dfs on root right first will help us to HAVE ample information 
-        //for next of left node.
-        //hence calling in this order is recommended.
-        dfs(root->right);
-        dfs(root->left);
-    }
     
-    Node* connect(Node* root) 
+    void populateRight(Node* root)
     {
-       if(!root) return nullptr;
-       root->next = nullptr;
-       dfs(root);
-       return root;
+        if(!root) return;
+        
+        
+        if(root -> left)
+        {
+            root -> left -> next = root -> right ? root -> right : findNext (root -> next);
+        }
+        
+        if(root -> right)
+        {
+           root -> right -> next = findNext(root -> next);           
+        }
+        
+        /*
+        if we are doing left recur first, then right recur 
+        If we have more than 4 levels, we may miss at some point to link next for right pointer 
+        for eg :
+                      1                          
+                   2      3                           
+                 4   5   6   7                       
+                8 N 1 0 N N 5 5                       
+                
+          so we are doing preorder, root -> left -> right
+          1, 2, 4, 8, 5, 1, 0, 6, 7, 5, 5
+          
+          at 1st time, we link 1's child 2 -> 3 , 3 -> null
+          at 2nd time, we link 2's child 4 -> 5 , 5 -> 6
+          at 3rd time, we link 8 with 5's left child 1 so, 8 -> 1 (because we already linked 4->5)
+          at 4th time, we link 5's child 1 -> 0, but to link 0 -> 5 (7th left child, we don't have link with 6 -> 7, so we can't reach 7) 
+          
+          so only we are doing recursion for right first and then left
+             
+        */
+        
+        populateRight(root -> right);
+        populateRight(root -> left);
+       
+    }
+   
+    
+    Node* connect(Node* root) {
+        if(!root) return NULL;
+        
+        populateRight(root);
+        
+        return root;
+    
     }
 };
