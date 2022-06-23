db920ebacdb7842d28b3237e96c35b46d03a2d1b
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-23 20:31:09 +0530

    Time: 509 ms (5.03%), Space: 73.9 MB (31.45%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,6 +10,10 @@
  */
 class Solution {
 public:
+    
+    // 1.Babbar bhaiya se hlka se smja 
+    // 2. Sbse pehle merge sort smj le like linked list 2 me divide hui fer age 2 me aise recursion se horah he last me bcha he single element and us element ko usko mergelist ke attach krdia dubara 
+    //     3.array ko normal divide kr skte he pr isko krne ke liye chaiye dimag mane toh fast and slow pointer chalake jahan jake slow ruka vahan tk ek linked list uske next yani temop->next ko krdia null hmne and baki dusra side yani slow se age ke kyuki temp pehle perform horha temp =slow and uske badh slow age bda toh temp ek peche chlra ais emanle and jais ehi mid pe aya slow vasse hi bngya kam and todo fodo
     ListNode* sortList(ListNode* head) {
         if(head==NULL ||head->next==NULL) return head;
        ListNode*temp=NULL;
@@ -52,44 +56,6 @@ public:
         }
         
         return ptr->next;}
-//         if(head==NULL || head->next==NULL)
-//             return head;
-//         ListNode* temp=NULL;
-//         ListNode* slow=head;
-//         ListNode* fast=head;
-        
-//         while(fast!=NULL && fast->next!=NULL){
-//             temp=slow;
-//             slow=slow->next;
-//             fast=fast->next->next;
-//         }
-//         temp->next=NULL;
-//         ListNode*L1=sortList(head);
-//         ListNode*L2=sortList(slow);
-// return mergelist(L1,L2);
-        
-        
-//     }
-//     ListNode *mergelist(ListNode*l1,ListNode*l2){
-//         ListNode*ptr=new ListNode(0);
-//         ListNode*curr=ptr;
-//         while(l1!=NULL &&l2!=NULL){
-//             if(l1->val<=l2->val){
-//                 curr->next=l1;
-//                 l1=l1->next;
-//             }else {
-//                 curr->next=l2;
-//                 l2=l2->next;
-//             }curr=curr->next;
-//         }
-//         if(l1!=NULL){
-//             curr->next=l1;
-//             l1=l1->next;
-//         }
-//         if(l2!=NULL){
-//             curr->next=l2;
-//             l2=l2->next;
-//         }
-//         return ptr->next;
+
     
 };
