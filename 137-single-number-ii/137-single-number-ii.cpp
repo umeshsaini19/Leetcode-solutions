0deae331c93eaf7775768e87f54dbfff587eb59d
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-23 09:40:28 +0530

    Time: 11 ms (67.50%), Space: 9.4 MB (98.73%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       int ones = 0;
-        int twos = 0;
+        int ans=0;
         
-        for ( int i : nums ) {
-            ones = (ones ^ i) & (~twos);
-            twos = (twos ^ i) & (~ones);
+        //codebix youtube channel 
+        for(int i=0;i<32;i++){ //32 bit hoti he total toh yeh voh 32 bit ko trvaerse krne ko
+            int sum=0;
+        for(int j=0;j<nums.size();j++){
+            if(((nums[j]>>i)& 1)==1)   //yeh check krne ke liye bit set ya ni agr set he count bd rha he jaise manlo count 4 hogya to 1 no extra he jiske krke count bd rha he
+                sum++;
         }
-        
-        return ones;
+            sum%=3;  //modulus krke ki kitne bde he 
+            if(sum!=0){
+                ans |= sum<<i;   //fir  voh ans vali bit ko set krdega agr count se modulus krne ke badh ans nikl jayega 
+            }
+        }
+        return ans;
     }
 };
-
-// ones - it will store the elements that appear once
-// twos - it will be storing the elements that are repeating twice
-// Now a dry run using the example: nums=[2,2,2,3]
-// here we have taken ones and twos initial values to be 0
-// now ones = (00^10)&(11) = 10 & 11 = 10 = 2 ( you can see that since 2 has appeared only once it is stored in ones)
-// twos = (00^10)&(01) = 10 & 01 = 00 = 0
-
-// ones = (10^10)&(11) = 00 & 11 = 00 = 0
-// twos = (00^10)&(11) = 10 & 11 = 10 = 2 ( you can see that since 2 has appeared twice so it is stored in twos now)
-
-// ones = (00^10)&(01) = 10 & 01 = 00 = 0
-// twos = (10^10)&(11) = 00 & 11 = 00 = 0
-
-// ones = (00^11)&(11) = 11 & 11 = 11 = 3
-// twos = (00^11)&(00) = 11 & 00 = 00 = 0
-
-// so now ones will be returned which is 3. So 3 here is the non-repeating element in the given array.
-// (Remember: There is no variable that stores the value of any element which occurs thrice.)
