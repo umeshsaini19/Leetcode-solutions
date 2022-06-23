cc6458b5fc71922721bc18bce2a96cbd697ff7b3
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-23 21:34:24 +0530

    Time: 22 ms (21.87%), Space: 11.5 MB (45.44%) - LeetHub

---
+++ 179-largest-number/179-largest-number.cpp
@@ -0,0 +1,30 @@
+bool compare(string a,string b){
+    //  cout<<a+b<<" "<<b+a;
+    return a+b > b+a;
+  
+}
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+	
+        vector<string> container;
+		
+        for(int i : nums) container.push_back(to_string(i));
+                        
+        for(int i=0;i<container.size();i++){
+            cout<<container[i]<<" ";
+        }
+        sort(container.begin(),container.end(),compare);
+            for(int i=0;i<container.size();i++){
+            cout<<container[i]<<" ";
+        }
+        
+        string result;
+        
+        for(int i=0;i<container.size();i++)  result+=container[i];
+        
+        return result[0]=='0'? "0" : result;
+    
+    }
+};
