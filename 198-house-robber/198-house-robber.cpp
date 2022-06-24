0135e79fa913a9b8a8f5d5d4d6b58dcc48a95207
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-24 10:49:41 +0530

    Time: 4 ms (33.98%), Space: 7.6 MB (75.25%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -10,11 +10,12 @@ public:
             int pick=nums[i];  
             
             if(i>1) {
-               pick=pick+prev;   //dp[i-2]=prev2;
+               pick=pick+prev;   //dp[i-2]=prev;   //0 prev2 me daldia toh jab 2 ayega toh 
             }
-            int not_pick=0+prev2;   //dp[i-1]= prev;
+            int not_pick=0+prev2;   //dp[i-1]= prev2;
             
            int  curri=max(pick,not_pick);
+          //jruri ni he ki ans hmesha pick hi de toh pick ko raja na manna har bari value bdl rhi he pick ke liye yeh last step most imp he 
             prev=prev2;
             prev2=curri;
         }
