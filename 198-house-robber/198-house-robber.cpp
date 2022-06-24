6ee5f69dcfe351a20d059a389c745a76fcbe92fe
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-24 10:43:24 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (75.25%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     
-    //peche se age ki trf bdh rhe he using recursion 
-    int solve(vector<int>& nums,int ind, vector<int>&dp){
-        // if(ind==0) return nums[0];  jaise yeh he same like tabulation me agr dp[0] he toh dp[0]=nums[0];
-        dp[0]=nums[0];
-        for(int i=1;i<ind;i++){
-            int pick=nums[i];
-            
-            if(i>1){
-                pick=pick+dp[i-2];
-            }
-         int not_pick=0+dp[i-1];
-            dp[i]=max(pick,not_pick);
-        }
-        return dp[ind-1];
-        
-        
-    }
+ 
     
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-         return solve(nums,n,dp);
+      int prev2=nums[0]; //jaise dp me tha starting me yeh krdo;
+        int prev=0;
+        for(int i=1;i<nums.size();i++){
+            int pick=nums[i];  
+            
+            if(i>1) {
+               pick=pick+prev;   //dp[i-2]=prev2;
+            }
+            int not_pick=0+prev2;   //dp[i-1]= prev;
+            
+           int  curri=max(pick,not_pick);
+            prev=prev2;
+            prev2=curri;
+        }
+        return prev2;
 	        
 	    }
     
