82960c3042a6453889132101cfad5bb7c43685f8
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-26 10:35:08 +0530

    Time: 3 ms (66.67%), Space: 6.4 MB (33.33%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.cpp
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -6,7 +6,7 @@ public:
         int count=0;
         for(int i=0;i<s.size();i++){
             if(count%2==0){                   //jaise yeh sidhi vali dandi ka count 0,2,4,6 arhi he vasse hi hmara ans arha he 
-                if(s[i]=='*') ans++;
+                if(s[i]=='*') ans++;//count jab 2 hoga mtlb hum 2 se 3 vale dbbe me agye he 
             }
  // 1 se 2 ke bech vale me ans bi bdega 2 se 3 vale me bdega 3 se 4 me ni vdgea
             if(s[i]=='|') count++;
