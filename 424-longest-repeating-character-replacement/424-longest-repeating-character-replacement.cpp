a331f4814e51d0ae7f28a8af88b820ce3dab4016
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-26 12:13:34 +0530

    Time: 20 ms (48.70%), Space: 7 MB (53.78%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        
+        //hmne map liya usme har ek window ki vlaidity check krte rhe and char dalte rhe jaise ki validity bang means isse bda substring na hga toh i plus hogya
+//         We traverse from each char of the string if the curr char exists on the map we increase the count else we put it in the map .
+// Now since we can only replace at most K no of items so if at any time the curr window has more than k replacable items we have to shrink that window .
           int n=s.size();
         int i=0,j=0,ans=-1;
         int maxi=0;
@@ -9,34 +13,18 @@ public:
             mp[s[j]]++;
             
             
-            maxi=max(maxi,mp[s[j]]);
-            if((j-i+1)-maxi>k){
+            maxi=max(maxi,mp[s[j]]);      //mp[j] yani map me check krte rhe hum values ko 
+            if((j-i+1)-maxi>k){  
                 mp[s[i]]--;
                     i++;
             }
-            ans=max(ans,j-i+1);
+            ans=max(ans,j-i+1);   //yeh fianl ans nikalne ke liye 
             j++;
         }
         return ans;
     }
 };
- // int n = s.size();
- //        int i = 0, j = 0, maxi = 0;
- //        unordered_map<char,int>mp;
- //        int ans = -1;
- //        while(j < n)
- //        {
- //            mp[s[j]]++;
- //            maxi = max(maxi, mp[s[j]]);
- //            if((j-i+1) - maxi > k){
- //                mp[s[i]]--;
- //                i++;
- //            }
- //            ans = max(ans, (j-i+1));
- //            j++;   
- //        }
- //        return ans;
-    //}
+
 //AABABBA k=1;
 // 1. mp---> A-1  maxi =1   if(1-1>1) false   --- ans=1 
 // 2. mp-> A-2   maxi = 2    false    ans =2 
