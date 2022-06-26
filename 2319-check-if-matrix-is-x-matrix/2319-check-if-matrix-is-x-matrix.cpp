63f2846a74f3c784f11f014fca4eed4bf3191fa0
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-26 10:12:03 +0530

    Time: 52 ms (33.33%), Space: 16.4 MB (33.33%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
@@ -7,6 +7,7 @@ public:
             for (int j=0;j<m;j++) {
                 if (i == j or j == m-i-1) {     //i==j for 0 ,0 and m-1 and m-1 diagonal ke liye 
                                                    // j=m-i-1== i=0 toh j=m-1 and jab j=m-1 toh i=0 in diagomal ke liye
+                    //yeh diagonal nikalne ki scheme hoti he
                         if (grid[i][j] == 0)
                             return false;
                 } else {
