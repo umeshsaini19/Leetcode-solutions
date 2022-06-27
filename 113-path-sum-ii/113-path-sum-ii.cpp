92223d91b27f377011bde83f012cfef586c8877a
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-27 17:56:04 +0530

    Time: 8 ms (90.19%), Space: 19.9 MB (61.07%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -11,25 +11,40 @@
  */
 class Solution {
 public:
-    void getAllPaths(TreeNode *root,int targetSum,vector<int> temp,vector<vector<int>> &ans)
-    {
-        if(!root)
-            return;
-        if(!root->left and !root->right and targetSum == root->val)
-        {
-           temp.push_back(root->val);   //yeh sirf last element ko add krne ke liye he 
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(root->val);   //yeh condition jab tk last me ni pucnhe chlti hi jarhi 
         
-        getAllPaths(root->left,targetSum-root->val,temp,ans);
-        getAllPaths(root->right,targetSum-root->val,temp,ans);
+    // vector to maintenance the nodes in the current path
+    vector<int> path;
+    // vector to save all paths that have sum equals to target
+    vector< vector<int> > ans;
+    
+    void dfs(TreeNode* curr, int sum,int target)
+    {
+        if(!curr)
+            return;
+        
+        sum += curr->val;
+        path.push_back(curr->val);
+        
+        // if curr Node is a leaf
+        if(!curr->left && !curr->right)
+        {
+            if(sum == target)            
+                ans.push_back(path);            
+        }
+        else
+        {
+            dfs(curr->left,sum,target);
+            dfs(curr->right,sum,target);
+        }
+        
+        path.pop_back();
     }
+    
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) 
     {
-        vector<vector<int>> ans;
-        getAllPaths(root,targetSum,{},ans);
+        ans.clear();
+        dfs(root,0,targetSum);
         return ans;
     }
+
 };
