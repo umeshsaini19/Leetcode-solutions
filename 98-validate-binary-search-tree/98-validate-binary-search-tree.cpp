8c9aa5b3c2862481b4c176bcf6d1dc7b822434c5
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-06-27 09:33:40 +0530

    Time: 15 ms (74.20%), Space: 21.6 MB (70.29%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,11 +11,22 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode*root,long minval,long  maxval){
-        if(root==NULL) return true;
-        if(root->val>=maxval|| root->val<=minval) return false;
-        return solve(root->left,minval,root->val) and solve(root->right,root->val,maxval);
+    bool solve(TreeNode* root,long min_val,long  max_val){
+              if(root==NULL) return true;
+        if(root->val>=max_val|| root->val<=min_val) return false;
+      
+        return (solve(root->left,min_val,root->val) and solve(root->right,root->val,max_val));  //jab left me jarhe he toh root valai value max hogi usse max na ho koi and jab right me jarhe toh root min value he usse chota na ho koi agr dono condition set toh apna ans v set
     }
     bool isValidBST(TreeNode* root) {
-        return solve(root, LONG_MIN , LONG_MAX);   }
+        return solve(root,LONG_MIN,LONG_MAX);
+        
+    }
 };
+// bool solve(TreeNode*root,long minval,long  maxval){
+//         if(root==NULL) return true;
+//         if(root->val>=maxval|| root->val<=minval) return false;
+//         return solve(root->left,minval,root->val) and solve(root->right,root->val,maxval);
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         return solve(root, LONG_MIN , LONG_MAX);   }
+// };
