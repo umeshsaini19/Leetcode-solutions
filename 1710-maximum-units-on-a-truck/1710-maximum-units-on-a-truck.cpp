7cf3240d3836279e6f82df0a0362395658273971
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-01 21:01:12 +0530

    Time: 57 ms (70.18%), Space: 16.9 MB (43.71%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -10,10 +10,16 @@ public:
         int ans=0;
         for(auto box: boxTypes){
             int x=min(box[0],truckSize);  //choose minimum boxes from available boxes and capacity left
-            ans+=(x*box[1]);  //adding units in ans
-            truckSize-=x;  //reduce the capacity
+           // cout<<x<<" ";
+            ans+=(x*box[1]); //adding units in ans
+           //  cout<<ans<<" ";
+           // cout<<box[1]<<" ";
+           truckSize-=x;  //reduce the capacity
             if(!truckSize) break;  //capacity full
         }
         return ans;
     }
 };
+
+//isme lgi he greedy approach jaise jis array me no of boxes jada he usko pehle hi lenge
+//and aise add krte jayenge and sort krdenge acc to second element //jaise truck me 1 jagah he toh 1*1 hoga last me first test case me 3 jagah hoti toh 3*1 hota 
