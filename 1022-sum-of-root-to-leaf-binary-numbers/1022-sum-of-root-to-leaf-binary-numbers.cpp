05efed92f7b98370e8add25a0e7a72f7776d3393
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-05 22:25:12 +0530

    Time: 7 ms (80.19%), Space: 16.6 MB (94.96%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -15,7 +15,7 @@ public:
     //pehla hi discuss me bda mst smjaya he
     int solve(TreeNode*root,int sum){
     if(root==NULL) return NULL;
-    sum=2*sum+root->val;
+    sum=2*sum+root->val;//yeh ek tarah se ulte arhe he hum upr se neche 100 arhe he toh 1 sbse pehle arha he toh aise likhna pdrha he 
         cout<<sum<<" ";
     if(!root->left and !root->right) return sum;
     
