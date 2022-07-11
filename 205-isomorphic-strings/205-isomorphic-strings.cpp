7cd273ab44d59b6c788ac50cee5557612d539fe5
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-11 21:23:46 +0530

    Time: 17 ms (22.25%), Space: 7.3 MB (11.54%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -14,14 +14,7 @@ public:
         return true;
     }
     bool isIsomorphic(string s, string t) {
-        return solve(s,t) and solve(t,s);
+        return solve(s,t) and solve(t,s);//yeh dono krne jruri he kyuki start me kisi ek vlue like badc and baba ke ecample mme b ke sath b hogya a ke sath pr d ke sath fir b hogya toh revere v check krnz jruri he
     }
 };
-   // map<char,char>mp;
-   //  for(int i=0;i<s.size();i++){
-   //      if(mp.find(s[i])!=mp.end()){
-   //          if(mp[s[i]]!=t[i]) return false;
-   //      }
-   //      else  mp[s[i]]=t[i];
-   //  }
-   //  return true;
+   //self explain he yr dono mapped ko dekh rhe he 
