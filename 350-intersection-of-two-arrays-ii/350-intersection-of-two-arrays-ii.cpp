d13850fa937b8dbd8d5eb5e7887a99643a512fca
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-12 13:56:11 +0530

    Time: 6 ms (76.78%), Space: 10.5 MB (37.11%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+public://simple jo hum dimag lgate he normal vahi he ki map me value dali and dusre se check krli ki yeh value ka count0 se bda he toh count kam krdo and usko ans vector me daldo
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
      unordered_map<int,int>mp;
         vector<int>ans;
@@ -8,8 +8,9 @@ public:
         }
         for(int i =0;i<nums2.size();i++){
             if(mp[nums2[i]]>0){
-                mp[nums2[i]]--;
                 ans.push_back(nums2[i]);
+                mp[nums2[i]]--;
+                
             }
         }
         return ans;
