36b56de17aa74794379baeb25bbf726d75339024
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-16 19:53:03 +0530

    Time: 51 ms (6.08%), Space: 6.6 MB (85.61%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -18,3 +18,16 @@ public:
         return min({b, a, l/2, o/2, n});   //yeh l/2 and o/2 esleye yuki 2 bari chaiye dono toh 2 bari hi hone wpprds me but cstring toh ek chaiye
     }
 };
+
+  // int b, a, l, o, n;
+  //       b = a = l = o = n = 0;
+  //       for (auto ch : text) {
+  //           switch (ch) {
+  //               case 'b' : b++; break;
+  //               case 'a' : a++; break;
+  //               case 'l' : l++; break;
+  //               case 'o' : o++; break;
+  //               case 'n' : n++; break;
+  //           }
+  //       }
+  //       return min({b, a, l/2, o/2, n});
