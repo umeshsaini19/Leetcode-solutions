c17a030267659e5d47741fb345bb9f99ea9bef5c
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-16 20:24:35 +0530

    Time: 4 ms (63.67%), Space: 8.2 MB (53.26%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -16,3 +16,31 @@ public:
 //dekho last line hi ni smj ayegi in future bhi toh last line ka mtlb he ki jaise map ke andr manlo 1->3,2->2,3->1 and set me gya 3,2,1,toh size equal he pr dekho agr 1->2 hota ad 2->2 and 3->1 toh set me bs 2,1 jata kyuki 2 repeat he toh size equl ni hota and game khtm 
 
 
+//yeh he mentos jindagi 
+
+//  vector<int> ans;
+//         int size = arr.size();
+//         int i = 0;
+//         sort(arr.begin(),arr.end());
+//         while(i<size){
+//             int count = 1;
+//             for(int j =i+1;j<size;j++){
+//                 if(arr[i]==arr[j]){
+//                     count++;
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//             ans.push_back(count);
+//             i = i+count;
+//         }
+//         size = ans.size();
+//         sort(ans.begin(),ans.end());
+//         for(int i = 0;i<size-1;i++){
+//             if(ans[i]==ans[i+1]){
+//                 return false;
+//             }
+//         }
+//         return true;   
+//     }
