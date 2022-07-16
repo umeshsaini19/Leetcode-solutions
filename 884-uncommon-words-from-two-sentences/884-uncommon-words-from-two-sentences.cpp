47cbb566bdb92154176b921cdc894df659dd4691
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-16 17:46:55 +0530

    Time: 8 ms (11.08%), Space: 6.9 MB (23.92%) - LeetHub

--- 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.cpp
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-   void uncommon(string s,unordered_map<string,int>&mp) {
-        string word="";
-        for(int i=0; i<s.size(); i++) {
-                if(s[i]==' ') {
-                     mp[word]++;
-                     word="";
-                }else {
-                     word+=s[i];
-                }
+    
+    void solve(string s,unordered_map<string,int>&mp){
+     string   word="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' '){
+                mp[word]++;
+            word="";}
+            else {
+                word+=s[i];
+            }
         }
         mp[word]++;
+        
     }
     vector<string> uncommonFromSentences(string s1, string s2) {
-        vector<string> ans;
-        unordered_map<string,int> mp;
-        uncommon(s1,mp);
-        uncommon(s2,mp);
-      for(auto it : mp){
-            if(it.second==1) ans.push_back(it.first);
+        vector<string>ans;
+        unordered_map<string,int>mp;
+        solve(s1,mp);
+        solve(s2,mp);
+        for(auto it:mp){
+            if(it.second==1)
+            ans.push_back(it.first);
         }
         return ans;
     }
 };
- 
+
