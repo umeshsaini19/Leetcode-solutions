b37f61210a858fe0b293a1794ee58fb8a6eed919
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-23 19:32:37 +0530

    Time: 181 ms (5.03%), Space: 26.1 MB (26.55%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    bool isPossible(int barrier, vector<int>&A, int N, int M)
+    {
+        int pages=0,student=1;
+        
+        for(int i=0;i<N;i++){
+            if(A[i]>barrier) return false;
+            if(pages+A[i]>barrier){
+                student+=1;        //jaise hi barrier bda studnt ka count bda diya and pages firse 0 and pages me plus shuru
+              pages=A[i];   // jaise 12 34 67 90 he toh jaise hi 67 aya first case me mid 107 tha toh vahan break hogya pr [ages ke andr value toh jayegi hi ]
+            }
+            else{
+                pages+=A[i];
+            }
+        }
+          if(student>M) return false;
+        else return true;}
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        int N=weights.size();
+         int ans=-1,low=INT_MAX;
+        int high=0;
+        for(int i=0;i<N;i++){
+            low=min(low,weights[i]),high+=weights[i];
+        }
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isPossible(mid,weights,N,days)){
+                ans=mid;
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+        }return ans;
+    }
+};
