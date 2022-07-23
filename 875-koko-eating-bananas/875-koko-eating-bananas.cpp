d90ec91633fa58b9762c1c8e2b74bc4d87d0998b
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-23 18:37:04 +0530

    Time: 52 ms (84.63%), Space: 19 MB (28.18%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+//     bool solve(vector<int>&piles ,int h,int k){
+//         int hour=0;
+//         for(int it:piles){
+//             int val=it/k;
+//             hour+=val;
+//             if(it%k!=0) hour++;
+//         }
+//         return hour<=h;
+//     }
+//     int minEatingSpeed(vector<int>& piles, int h) {
+//         int low=1;
+//         int high=1000000000;
+//         while(low<=high){
+//             int mid=low+(high-low)/2;
+//             if(solve(piles,h,mid)) high=mid-1;
+//             else 
+//                 low=mid+1;
+//         }
+//         return low;
+//     }
+// };
+        
+        
+        
+          int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = 1000000000;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(canEatInTime(piles, mid, h)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
+    }
+    bool canEatInTime(vector<int>& piles, int k, int h){
+        long hours = 0;
+        for(int pile : piles){
+             int div = pile / k;
+            hours += div;
+            if(pile % k != 0) hours++;
+        }
+        return hours <= h;
+    
+    }
+};
