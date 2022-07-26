ce1f31c9cb60f82a1f65fbe82e1cbcfaaccaa190
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-07-26 19:40:03 +0530

    Time: 4 ms (24.85%), Space: 6 MB (65.85%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -7,8 +7,8 @@ public://yeh codeix se sikha he
         
         int res{};
         
-        for(int i{1}; i<=n; ++i)
-            res += solve(i-1, dp)*solve(n-i, dp);
+        for(int i=1; i<=n; ++i)
+            res += solve(i-1, dp)*solve(n-i, dp);   //  1 2 3 4 5 start se leke i-1 tk ka btado and i+1 yani root se leke last tk ka btado jo bhi ans ayega use res me add krdo
         
         return dp[n] = res;
     }
