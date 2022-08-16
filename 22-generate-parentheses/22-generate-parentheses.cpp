843b7a38b60b0c1340020e1f2e77ad85a69bfd4c
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-08-16 08:40:44 +0530

    Time: 4 ms (74.31%), Space: 13.7 MB (48.15%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
-    void solve(int open,int close,string op,vector<string>&ans){
-        
-        ///jaise tree diagram bnaya ek bath pta lgi leaf yani jab dono khtm honge tab output milri and open bracket toh hmesh arhi hi he jab tk zero na hho pr close bracket kbhi ati kabhi ni toh close bracket tbhi ayegi jab close bracket ka count open se jada hoga tb dono possibilit hogi open v ayei and close bhi 
-        if(open==0 and close==0)
-        {
+    void solve(int open,int close,string &op,vector<string>&ans){
+        if(open==0 and close==0){
             ans.push_back(op);
-            return;
-        }
+            return; }
         if(open!=0){
             string op1=op;
             op1.push_back('(');
@@ -18,10 +14,9 @@ public:
             op2.push_back(')');
             solve(open,close-1,op2,ans);
         }
-        return ;
     }
     vector<string> generateParenthesis(int n) {
-    vector<string>ans;
+        vector<string>ans;
         string op="";
         int open =n;
         int close=n;
@@ -29,3 +24,33 @@ public:
         return ans;
     }
 };
+
+
+        
+        
+//         if(open==0 and close==0)
+//         {
+//             ans.push_back(op);
+//             return;
+//         }
+//         if(open!=0){
+//             string op1=op;
+//             op1.push_back('(');
+//             solve(open-1,close,op1,ans);
+//         }
+//         if(close>open){
+//             string op2=op;
+//             op2.push_back(')');
+//             solve(open,close-1,op2,ans);
+//         }
+//         return ;
+//     }
+//     vector<string> generateParenthesis(int n) {
+//     vector<string>ans;
+//         string op="";
+//         int open =n;
+//         int close=n;
+//         solve(open,close,op,ans);
+//         return ans;
+//     }
+// };
