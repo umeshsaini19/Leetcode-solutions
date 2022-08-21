33a1eb60903c85293b90ec92bbd24595b03d3774
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-08-21 18:08:03 +0530

    Time: 389 ms (21.17%), Space: 113.8 MB (17.75%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -18,3 +18,5 @@ public:
         return false;
     }
 };
+
+//dekh simple jo hum two sum pair sum vale krte he logic same he uska he que yeh ki atleast 2 continours position check kro and dekh ki unka sum k ke multiple he ya ni so hmne shuru se check kiya and agr direct sum se milra k ka multiply toh check krlia and return true and ni milra toh jo rem ab aya voh agr pehle bhi aya he toh bech ki jo value he voh k ka multiple hi he ya k khud ka jaise normal two sum me hota he ki sum dubara a jaye k se minus krne me toh pta lg jata he ki k pehle aya tha
