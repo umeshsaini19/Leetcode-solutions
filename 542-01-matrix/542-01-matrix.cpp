c8d8bac8a07b94792995aca6ed560cd7578e4723
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-08-29 18:13:47 +0530

    Time: 184 ms (18.38%), Space: 30.4 MB (42.21%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool check(int i,int j,int row,int col){
+        if(i<0||j<0||i>row-1||j>col-1) return false;
+        else return true;
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,-1));
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({i,j});
+                    ans[i][j]=0;
+                }
+            }
+        }
+        int row=n;
+        int col=m;
+        while(!q.empty()){
+            int i=q.front().first;
+            int j=q.front().second;
+            
+            if(check(i+1,j,row,col) and ans[i+1][j]==-1)
+            {
+                q.push({i+1,j});
+                ans[i+1][j]=ans[i][j]+1;
+            }
+            
+             if(check(i-1,j,row,col) and ans[i-1][j]==-1)
+            {
+                q.push({i-1,j});
+                ans[i-1][j]=ans[i][j]+1;
+            }
+             if(check(i,j+1,row,col) and ans[i][j+1]==-1)
+            {
+                q.push({i,j+1});
+                ans[i][j+1]=ans[i][j]+1;
+            } if(check(i,j-1,row,col) and ans[i][j-1]==-1)
+            {
+                q.push({i,j-1});
+                ans[i][j-1]=ans[i][j]+1;
+            }
+            q.pop();
+        }
+        return ans;
+    }
+};
