f924484025c7a9aab994c5c55594f2773e04197a
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-09-02 17:28:33 +0530

    Time: 42 ms (43.14%), Space: 13.8 MB (48.79%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -36,4 +36,39 @@ public:
     }
 };
 
+
+//yeh second method se bhi kiya gya he yeh question
+//  vector <int> parent;
+//     int findCircleNum(vector<vector<int>>& is) {
+//         int v=is.size();
+//         parent.resize(v, -1);
+        
+//         for(int i=0; i<v; i++)
+//             for(int j=i+1; j<v; j++)
+//                 if(is[i][j]==1)
+//                     unionn(i, j);
+        
+//         int count=0;
+//         for(auto it: parent){
+//             if(it == -1)
+//                 count++;
+//         }
+//         return count;
+//     }
+    
+//     void unionn(int i, int j){
+//         i = find(i);
+//         j = find(j);
+//         if(i != j)
+//             parent[i]=j;
+//     }
+    
+//     int find(int v){
+//         if(parent[v]==-1)
+//             return v;
+//         else
+//             return find(parent[v]);
+//     }
+// };
+
 //same striver bhaiy VALA HE NOTES ME BHI HE VIDEO BHI
