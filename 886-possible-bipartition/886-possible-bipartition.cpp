4b0359ee5a49c06a3b0e4e71596a134205c13fe4
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-09-04 12:54:25 +0530

    Time: 538 ms (6.50%), Space: 84.8 MB (5.01%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
- bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<int> G[n+1];
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        vector<int>graph[n+1];  //yeh graph bnaya n+1 ka esleye kyuki 0 based indexing he mere bhai 
         
-        for(auto p: dislikes) {
-            int u = p[0], v = p[1];
-            G[u].push_back(v);
-            G[v].push_back(u);
+        for(auto it: dislikes){  //jo given disklikes he unko ek graph me daldo ki badh me jab fill krne lge colour array toh direct check krle kisko konca ni psnd
+            int u=it[0],v=it[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);  //1 ko 3 ni psnd toh 3 ko 1 bhi ni psnd
+            
+            
         }
         
-        vector<int> colour(n+1, -1);        
-        for(int i=1;i<=n;i++) {
-            queue<int> q;
-            if(colour[i] == -1) {
-                colour[i] = 0;
-                q.push(i);
-            }
-            while(!q.empty()) {
-                int u = q.front();
+        vector<int>color(n+1,-1);
+        for(int i=1;i<=n;i++){
+        queue<int>q;
+            if(color[i]==-1){
+                color[i]=0; 
+                //pehla color dedia
+            q.push(i);
+              //  cout<<q.top();
+            }//yeh yahan tk first value queue
+            while(!q.empty()){
+                int node=q.front();
                 q.pop();
-                for(int v: G[u]) {
-                    if(colour[v] == -1) {
-                        colour[v] = 1-colour[u];
-                        q.push(v);
-                    } else if(colour[u] == colour[v]) {
-                        return false;
+                for(auto it :graph[node]){
+                    if(color[it]==-1)
+                    {
+                        color[it]=1-color[node];
+                        q.push(it);
                     }
+                    else if(color[it]==color[node])
+                        return false;
                 }
             }
         }
-        return true;
-    } 
+            return true;
+    }
+        
 };
+
+
+// striver bhaiya ki bipartite ki video he and apne notes he is same que ka code codebix se smja he baki jrurat v ni he jada 
+
+// dekh simple bfs lgaya and me khud ko first colour dekhke mere jo jo neighbour he jo muje psnd ni he unko dusra colour derha hu and in the end agr jisko jo jo neighbour psnd ni he sab apni mrji ke acc fit huye toh return true 
