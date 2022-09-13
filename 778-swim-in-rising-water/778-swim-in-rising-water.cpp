8cf2474813662fbaf40aef6d56d6b4fa7ea27d6d
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-09-13 09:08:14 +0530

    Time: 31 ms (64.00%), Space: 8.6 MB (95.16%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.cpp
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -1,65 +1,34 @@
 class Solution {
 public:
-    
-      bool isSafe(int x, int y, int n){
-        if(x>=0 && x<n && y>=0 && y<n){
-            return true;
-        }
-        return false;
-    }
     int swimInWater(vector<vector<int>>& grid) {
-         priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> q;
-        
-    vector<vector<int>> dir= {{0,-1}, {-1,0}, {0,1}, {1,0}};
-    int ans=0;
-    int n=grid.size();
-    int m=grid[0].size();
-          vector<int> visited(n*n, false); 
-     
-        q.push(make_pair(grid[0][0], make_pair(0,0)));
-          visited[grid[0][0]] = true;
-              pair<int, pair<int,int>> res;
-    while(q.size())
-    {
-        auto res=q.top();
-        q.pop();
-        ans=max(ans,res.first);
-        
-        int x=res.second.first;
-        int y=res.second.second;
-        
-    
-            
-        
-        if(x == n-1 && y == m-1){
-                break;
-            }
-        
-       // move to next row
-             if(isSafe(x+1,y,n) && visited[grid[x+1][y]]==false){
-                visited[grid[x+1][y]] = true;
-                q.push(make_pair(grid[x+1][y], make_pair(x+1,y)));
-            }
-            // move to previous row
-            if(isSafe(x-1,y,n) && visited[grid[x-1][y]]==false){
-                visited[grid[x-1][y]] = true;
-                q.push(make_pair(grid[x-1][y], make_pair(x-1,y)));
-            }
-            // move to next column
-            if(isSafe(x,y+1,n) && visited[grid[x][y+1]]==false){
-                visited[grid[x][y+1]] = true;
-                q.push(make_pair(grid[x][y+1], make_pair(x,y+1)));
-            }
-            // move to previous column
-            if(isSafe(x,y-1,n) && visited[grid[x][y-1]]==false){
-                visited[grid[x][y-1]] = true;
-                q.push(make_pair(grid[x][y-1], make_pair(x,y-1)));
+       priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        int n = grid.size(), m = grid[0].size();
+        int dirx[4] = {1, -1, 0, 0};
+        int diry[4] = {0, 0, -1, 1};
+        int val = 0;
+        pq.push({grid[0][0],{0,0}});
+        grid[0][0] = -1;
+        while(!pq.empty())
+        {
+            int sz = pq.size();
+            while(sz--)
+            {
+                pair<int, pair<int, int>> tp = pq.top();
+                int x = tp.second.first, y = tp.second.second;
+                pq.pop();
+                val = max(val, tp.first);
+                if(x == n-1 && y == m-1) return val;
+                for(int i = 0; i < 4; i++)
+                {
+                    int nxtx = x + dirx[i];
+                    int nxty = y + diry[i];
+                    if(nxtx < 0 || nxtx >= n || nxty < 0 || nxty >= m) continue;
+                    if(grid[nxtx][nxty] == -1) continue;
+                    pq.push({grid[nxtx][nxty],{nxtx, nxty}});
+                    grid[nxtx][nxty] = -1;
+                }
             }
         }
-        
-        return ans;
+        return val;
     }
-       
-    
-    
 };
