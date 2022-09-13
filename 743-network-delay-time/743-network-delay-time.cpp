cf4d7dd610c67e3e9816905eb50a62f055606848
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-09-13 13:39:17 +0530

    Time: 2920 ms (5.00%), Space: 271.7 MB (5.05%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -6,7 +6,7 @@ public:
      
      for(int i=0;i<n-1;i++)
      {
-         bool flag=false;
+        
          for(auto node:times)
          {
              int src=node[0];
@@ -15,13 +15,10 @@ public:
              if(dist[src]!=INT_MAX&&dist[des]>dist[src]+time)
              {
                  dist[des]=dist[src]+time;
-                 flag=true;
+                
              }
          }
-        // if(flag==false)
-        //      break;
      
-         
      }
      int res=0;
      for(int i=1;i<=n;i++)
@@ -33,3 +30,5 @@ public:
      return res;
  }
 };
+
+//yeh que bellmon and djkistra dono se kiya he and bs ho jayega easy
