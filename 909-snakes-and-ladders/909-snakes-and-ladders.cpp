9f9bb774f35ccb5c7fd95cf20246b9c036773dea
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-09-20 11:48:21 +0530

    Time: 30 ms (62.99%), Space: 12.8 MB (72.79%) - LeetHub

---
+++ 909-snakes-and-ladders/909-snakes-and-ladders.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n=board.size();
+        vector<vector<bool>>visited(n,vector<bool>(n,false));
+        
+        queue<int>q;
+        q.push(1);
+        visited[n-1][0]=true;    //yeh last row and first column yani ki first position ko visited krdia 
+        int steps=0;
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                int curpos=q.front();
+                if(curpos==n*n) return steps; // last position pe agye he 
+                q.pop();
+            for(int i=1;i<=6;i++){
+            int nextpos=curpos+i;
+                if(nextpos>n*n) break;//last pe punch gya add krte krte tohbreak krde 
+                int r=n-(nextpos-1)/n-1;   //yeh row nikalne ka formyla he 
+                    int c=(nextpos-1)%n; //yeh column nikalne ka agr even length he toh  row ka
+                if(r%2==n%2) c=n-c-1; //agr odd length he bcz of zig zag type 
+                if(!visited[r][c]){
+                    visited[r][c]=true;
+                    if(board[r][c]!=-1) //means ya toh ladder he he ya stairs toh board pe jp value he voh deni  hogi{
+                        q.push(board[r][c]);
+                    
+                else q.push(nextpos);
+                }
+            }
+        }
+             steps++;
+        }
+       
+        
+        
+        return -1;
+    }
+};
+
+//codebix se smja he yeh code 
