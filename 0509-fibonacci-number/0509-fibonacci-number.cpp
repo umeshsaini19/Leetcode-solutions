dfad2bb19ba30f505cd1b63421b3b662924db33e
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-10-14 09:16:59 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (95.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
    int fib(int n) {
-        int ans[n+2];
+        int ans[n+2];   //yeh ab n+2 elese kyuki edge case me input 0 ho skte he hum neche decalre kr rhe he arr[0]=0 and arr[1]=1 and agr 0 agya and n+1 huya toh 0+1 hi huya and neche vala kam ni krega and error dega esleye 
         ans[0]=0;
         ans[1]=1;
         
