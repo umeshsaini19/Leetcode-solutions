d41b2d2bc54adecc4b388c755aed82b6763f8b20
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-10-19 16:57:01 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (49.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,3 +10,5 @@ public:
         return dp[n-1][m-1];
     }
 };
+
+//dekh ya toh upr vala jo method he vasse krle ki pehle hi sbko one dede and fir add krta ja ya fer base cond add krde ki if i==0 || j==0 toh bhi dp[n][m]==1 krde kyuki 0th row 0th column me hmesha 1 hi path hoga toh us time pe i =0 and j=0  se cond hogi and  if me dp[i][j]==1 else me dp[i][j]=dp[i-1][j]+dp[i][j-1] eslai bcz suppose 5,5 he toh 4,5 vala path plus 5,4 vala path dono add krke ans
