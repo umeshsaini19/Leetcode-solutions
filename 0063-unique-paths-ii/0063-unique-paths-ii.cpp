d04a2b2167ac342da0e5acee886a577cb0b30d80
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-10-22 11:09:30 +0530

    Time: 3 ms (88.72%), Space: 7.9 MB (13.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     
-   int m, n;
-vector<vector<int> > dp;
-int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-	m = size(obstacleGrid), n = size(obstacleGrid[0]);
-	dp.resize(m, vector<int>(n));
-	return solve(obstacleGrid, 0, 0);
-}
-// function to recursively explore all unique paths and store the results once calculated
-int solve(vector<vector<int>>& grid, int i, int j) {
-	if(i < 0 || j < 0 || i >= m || j >= n) return 0;    // bounds checking
-	if(grid[i][j]) return  0;                 // obstacle found at current cell
-	if(i == m - 1 && j == n - 1) return 1;              // reached bottom-right of grid ? return 1
-	if(dp[i][j]) return dp[i][j];                       // if already computed for current cell, just return the stored results
-	return dp[i][j] = solve(grid, i + 1, j) + solve(grid, i, j + 1); // recursively explore the two options available with us
+ int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+	int m = size(grid), n = size(grid[0]);
+	vector<vector<int> > dp (m + 1, vector<int>(n + 1));
+    dp[0][1] = 1;
+    for(int i = 1; i <= m; i++)
+        for(int j = 1; j <= n; j++)            
+			// dp[i][j] = sum of unique paths for top and left cell (cells from which we reach current one)
+            dp[i][j] = !grid[i - 1][j - 1] ? dp[i - 1][j] + dp[i][j - 1] : 0;
+    return dp[m][n];
 }
 };
-
 //same he unique path 1 vala bs ek condition jo grid[i][j] vali he voh ad hogi he
