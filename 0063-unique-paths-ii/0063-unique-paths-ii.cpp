2a94c6edb621f683a6c2033c0a0a9c0fb1a1ac1e
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-10-22 11:58:50 +0530

    Time: 7 ms (49.41%), Space: 7.7 MB (68.35%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
-    
- int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-	int m = size(grid), n = size(grid[0]);
-	vector<vector<int> > dp (m + 1, vector<int>(n + 1));
-    dp[0][1] = 1;
-    for(int i = 1; i <= m; i++)
-        for(int j = 1; j <= n; j++)            
-			// dp[i][j] = sum of unique paths for top and left cell (cells from which we reach current one)
-            dp[i][j] = !grid[i - 1][j - 1] ? dp[i - 1][j] + dp[i][j - 1] : 0;
-    return dp[m][n];
-}
+ int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>>dp(m, vector<int>(n, 0));
+        
+        //filling 0th row
+        for(int i = 0; i < n; i++){
+            if(obstacleGrid[0][i] == 1) break;
+            dp[0][i] = 1;
+        }
+        
+        //filling 0th column
+         for(int i = 0; i < m; i++){
+            if(obstacleGrid[i][0] == 1)  break;   //else ki jaise hi one agya first row ya first column toh chance hi ni ki frst row se usse age ja payenge hum log
+            dp[i][0] = 1;
+        }
+        
+        //filling rest of the dp array
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++){
+                if(obstacleGrid[i][j] == 1) continue;
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        return dp[m-1][n-1];
+    }
 };
-//same he unique path 1 vala bs ek condition jo grid[i][j] vali he voh ad hogi he
+
+//jo pehli row na dpehla colm he bs voh hum apne dimag se bhr skte he else sbke liye hme plus minus krna pdega
