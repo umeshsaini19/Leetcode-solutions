dc54ab440c65a221ef5401b062c69bbf573f7a58
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-10-26 16:53:02 +0530

    Time: 19 ms (68.05%), Space: 7.3 MB (84.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -7,28 +7,26 @@ public:
         return editDistance(s, t, m, n);
     }
     
-    int editDistance(string& word1, string& word2, int m, int n){
-		
-	  int dp[m+1][n+1];
-        for(int i=0;i<m+1;i++){
-            for(int j=0;j<n+1;j++){
-                if(i==0)dp[i][j]=j;
-                if(j==0)dp[i][j]=i;
-            }
+    int editDistance(string& S1, string& S2, int n, int m){
+	 int dp[n+1][m+1];
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<m+1;j++){
+            if(i==0) dp[i][j]=j;//jitne jada j honge utne steps chaoye extra i me tbhhi equals to j
+            if(j==0) dp[i][j]=i;//same for i
         }
-    for(int i=1;i<m+1;i++){
-        for(int j=1;j<n+1;j++){
-    
-        if(word1[i-1] == word2[j-1]) {
-			dp[i][j]=dp[i-1][j-1];}
-        else{
-            int insertChar = dp[i][j-1]+1;
-            int deleteChar = dp[i-1][j]+1;
-            int replaceChar = dp[i-1][j-1]+1;
-            
-          dp[i][j]=  min(insertChar, min(deleteChar, replaceChar));}
-    }}
-          return dp[m][n];
     }
-            
+        
+  for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+        
+    if(S1[i-1]==S2[j-1]){
+        dp[i][j]=dp[i-1][j-1];
+        
+    }
+    else {
+        dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+    }
+        }
+  }
+    return dp[n][m];}
 };
