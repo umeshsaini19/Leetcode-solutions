e6ebdf04870d37cd68623749c280cf72ca3e997b
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-11-07 19:34:34 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (53.07%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(string digits,string output,int index,vector<string>&ans,string mapping[]){
+        if(index>=digits.size())
+        {
+            ans.push_back(output);
+            return;
+        }
+        int number=digits[index]-'0'; //kyuki hme exact no chaiye 2 3 kyuki string array me number se hi pta chlega
+        string value=mapping[number];
+        for(int i=0;i<value.length();i++){
+            output.push_back(value[i]);
+            solve(digits,output,index+1,ans,mapping);
+            output.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string>ans;
+        if(digits.length()==0) return ans;
+        int index=0;
+        string output="";
+        string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits,output,index,ans,mapping);
+        return ans;
+    }
+};
