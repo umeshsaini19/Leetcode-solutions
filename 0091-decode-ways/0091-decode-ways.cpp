bfc12e84f657209df95476b1ffb831230ad18d6f
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-11-15 21:26:36 +0530

    Time: 7 ms (26.95%), Space: 6.1 MB (91.18%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-  
+    
     int dp[102];
-    int solve(string& s, int idx,int n)
+    int decode(string& s, int idx, int n)
     {
-        int ways=0;
-        if(idx<n and s[idx]=='0' ) return 0;   //jaise single index pe 0 ayega toh voh koi kam ka ni he bcz voh result me partiipate ni krega
-        if(idx>=n) return 1; //hum last index me punch gye recursion tree se toh ab 1 result agya he //chahe 1 1 element lelo ya 1 2 aise yeh jaise h recursion tree me jtini bari last me punche utne me hogya kam
-            if(dp[idx]!=-1) return dp[idx];
+        if(idx < n && s[idx] == '0') return 0;
+        if(idx >= n)
+            return 1;
         
-        if(dp[idx]!='0') ways+=solve(s,idx+1,n);
+        if(dp[idx] != -1) return dp[idx];
         
+        int ways = 0;
+        
+        // Pick single
+        if(s[idx] != '0') ways+= decode(s, idx+1, n);
+            
         // Pick couple
         if(idx+1 < n && ((s[idx] == '1' && s[idx+1] <= '9') || (s[idx]=='2' && s[idx+1] < '7')))
-           ways += solve(s, idx+2, n);
-        
-        return dp[idx]=ways;
-        
-    }   
+           ways += decode(s, idx+2, n);
+           
+        return dp[idx] = ways;
+    }
     
     int numDecodings(string s) {
-       int n=s.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(s,0,n);
+       
+        int n = s.size();
+        memset(dp, -1, sizeof(dp));
+        return decode(s, 0, n);
     }
 };
+
+//vasse que diff ni he aram se dekhne se smj a jayega isse pehle vale sub,ission me thode cmnd and ek soch rha hoga ki s[idx]==0 pe 0 return kyu he toh  jaise "01 " hoga toh 0 ka kuch existance ni he ab tu sochega ki 20 ka toh existemce toh smj ki hme value ni return krni he hme sirf count return krna he toh 20 ka count usne 1 leliye he and +2 krdia he pick couple ke andr
