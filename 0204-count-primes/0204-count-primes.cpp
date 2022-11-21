2d2ea2acc555e7e158d56cfc26203e3b61257cf1
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-11-21 10:24:40 +0530

    Time: 190 ms (93.85%), Space: 10.3 MB (69.35%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -23,3 +23,5 @@ public:
         return count;
     }
 };
+
+//anuj bhaiya se sieve of erathaisnos jo bhi he voh smja he usme yehi he ki jaise hme 14 se pehle tk ke check krne he toh  hum under root 14 tak i run krenge and o(n) se o(under root n  ) ho jayegi complexity kyuki jaise 7 he uska hi last multiple ho skta he 14 uske baad toh koi aise value ni hogi jisse check kr skte he aur jo 14 he voh hum ne 2 se pehle hi check krlia he so no need and ek baat aur second loop ka kam he ki jaise hme 2 ke multiple ko vector me false krne he esleye 2*i toh 4 se start krnege and j+=i means 4+=2 means har bari sidha gap 2 ka hi mila and last me sidha count krlia he and 0,1 non primes he so unko pehle hi false krdia he hmne
