88cbd97ee8afbca3498925748c1c88a92e4d0bd6
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-11-23 09:27:19 +0530

    Time: 18 ms (96.75%), Space: 17.7 MB (66.73%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
-     void wiggleSort(vector<int>& nums) {
-        int n= nums.size();
-         priority_queue<int>q(nums.begin(),nums.end());
+//      void wiggleSort(vector<int>& nums) {
+//         int n= nums.size();
+//          priority_queue<int>q(nums.begin(),nums.end());
          
-         for(int i=1;i<n;i+=2){
-             nums[i]=q.top();
-             q.pop();
-         }
+//          for(int i=1;i<n;i+=2){
+//              nums[i]=q.top();
+//              q.pop();
+//          }
          
-          for(int i=0;i<n;i+=2){
-             nums[i]=q.top();
-             q.pop();
-         }
+//           for(int i=0;i<n;i+=2){
+//              nums[i]=q.top();
+//              q.pop();
+//          }
          
-         
-         
-         
+         //2 solution krdiye he isme dono bdia he dekhlio 
          
          
          
-         
-         
-         
-         
-//         priority_queue<int> q(nums.begin(), nums.end());
-//         for(int i=1; i<n; i += 2){
-//             nums[i] = q.top();
-//             q.pop();
-//         }
-//         for(int i=0; i<n; i += 2){
-//             nums[i] = q.top();
-//             cout<<nums[i]<<" ";
-//             q.pop();
-//         }
+ void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        
+        vector<int> temp(n);
+        int i = 1, j = n-1;
+        while(i <nums.size()){
+            temp[i] = nums[j];
+            i+=2;
+            j--;
+        }
+        i = 0;
+        while(i<nums.size()){
+            temp[i] = nums[j];
+            i+=2;
+            j--;
+        }
+        for(int it =0; it<n;it++){
+            nums[it] = temp[it];
+        }
     }
 };
