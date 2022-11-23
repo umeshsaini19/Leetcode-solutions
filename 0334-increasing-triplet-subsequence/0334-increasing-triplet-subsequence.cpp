be1612ba61dd5e53e81b22a38f5eebb6badbaf9b
Umesh saini <90331257+umeshsaini19@users.noreply.github.com>
2022-11-23 17:02:35 +0530

    Time: 188 ms (18.64%), Space: 109.7 MB (6.55%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -29,3 +29,13 @@ public:
 };//yeh hi ek method he upr vala brute force neche vala optimal bs jo condition he vahi check krlo and ans a jeyag jayaz c baathe cond bhi aisi he que e acc hi 
 
 //brute force
+
+// low<mid<high
+// our intension is that make low smaller as can possible
+// make mid smaller as can possible but it should be greater than low
+// if we found any element greater than mid than we get triplet
+// 1.Traverse whole array
+// 2.if nums[i] <low means we can update low bcz we found new low .
+// 3.if nums[i] >low && nums[i]<mid it will be new mid
+// 4.if nums[i] >mid it means nums[i] is largest than mid and it is als greater than low
+// so we get nums[i] as high so return true
